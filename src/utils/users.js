1609644de283d183dfc0c00183a6f5c25703d436
len23 <lenin.montalvo.23@hotmail.com>
2020-06-06 17:26:47 -0500

    First Commit

---
+++ src/utils/users.js
@@ -0,0 +1,60 @@
+const users=[];
+
+// adduser,removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) =>{
+  // Clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+  
+  //Validate the data
+  if(!username || !room){
+    return {
+      error: 'Username and room are required!'
+    }
+  }
+
+  // Check for existing user
+  const existingUser = users.find((user)=>{
+    return user.room === room && user.username === username;
+  })
+
+  // Validate username
+  if (existingUser) {
+    return{
+      error: 'Username is in use'
+    }
+  }
+
+  //Store user
+  const user = { id, username, room };
+  users.push(user);
+  return { user }
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex(user=>id===user.id);
+
+  if(index>=0){
+    return users.splice(index, 1)[0]
+  }
+}
+
+const getUser = (id)=>{
+  return user = users.find(user=>user.id===id);
+}
+const getUsersInRoom = (roomName)=>{
+  const usersRoom = users.filter(user=>{
+    return user.room  === roomName.trim().toLowerCase();
+  })
+  return usersRoom;
+}
+
+module.exports= {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
+
+
